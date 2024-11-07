69ba7fdd41d2b6841e17c9d4c72899482930761f
jPales1 <joaopedropales@gmail.com>
2024-11-06 23:33:35 -0300

    adicionar botão de logout e integrar serviço de lista de compras para gerenciar itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from '@auth0/auth0-angular';
 import { Item } from './item.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-root',
@@ -17,24 +18,48 @@ import { Item } from './item.model';
 export class AppComponent {
   title = 'lista-supermercado';
   items: Item[] = [];
-  nextId = 1;
 
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private shoppingListService: ShoppingListService) {
+    this.loadItems();
+  }
+
+  loadItems() {
+    this.shoppingListService.getItems().subscribe(items => {
+      this.items = items;
+    });
+  }
 
   addItem(itemName: string) {
     if (itemName) {
-      this.items.push({ id: this.nextId++, name: itemName, isPurchased: false });
+      const newItem: Partial<Item> = { name: itemName, isPurchased: false };
+      this.shoppingListService.addItem(newItem as Item).subscribe(() => {
+        this.loadItems(); // Recarrega a lista de itens do backend
+      });
     }
   }
 
   removeItem(id: number) {
-    this.items = this.items.filter(item => item.id !== id);
+    this.shoppingListService.removeItem(id).subscribe(() => {
+      this.items = this.items.filter(item => item.id !== id); // Atualize a lista de itens no frontend
+    });
   }
 
   toggleItemPurchased(id: number): void {
     const item = this.items.find(item => item.id === id);
     if (item) {
       item.isPurchased = !item.isPurchased;
+      this.shoppingListService.updateItem(item).subscribe(updatedItem => {
+        // Atualize o item na lista local se necessário
+        const index = this.items.findIndex(i => i.id === updatedItem.id);
+        if (index !== -1) {
+          this.items[index] = updatedItem;
+        }
+      });
     }
+    this.loadItems();
+  }
+
+  logout() {
+    this.auth.logout({ logoutParams: { returnTo: window.location.origin } });
   }
 }
