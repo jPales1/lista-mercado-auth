532ccfc359ea37cc3acc62378723c3ca48e4ba45
jPales1 <joaopedropales@gmail.com>
2024-11-06 23:33:16 -0300

    adicionar estilo e estrutura ao componente de login, implementando lógica para adicionar usuários ao serviço de lista de compras

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '@auth0/auth0-angular';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule],
-  template: `<button (click)="loginWithRedirect()">Login com Auth0</button>`
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  constructor(public auth: AuthService) {}
+export class LoginComponent implements OnInit {
+  constructor(public auth: AuthService, private shoppingListService: ShoppingListService) {}
 
   loginWithRedirect() {
     this.auth.loginWithRedirect();
+  }
+
+  ngOnInit() {
+    this.auth.user$.subscribe(user => {
+      if (user) {
+        this.shoppingListService.getUser(user.sub || '').subscribe(
+          existingUser => {
+            if (!existingUser) {
+              this.shoppingListService.addUser({
+                id: user.sub || '',
+                name: user.name || '',
+                email: user.email || ''
+              }).subscribe();
+            }
+          },
+          error => {
+            if (error.status === 404) {
+              this.shoppingListService.addUser({
+                id: user.sub || '',
+                name: user.name || '',
+                email: user.email || ''
+              }).subscribe();
+            }
+          }
+        );
+      }
+    });
   }
 }
