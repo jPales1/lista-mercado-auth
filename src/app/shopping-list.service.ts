0e2f55f1dc576a2c8f93a4c9d7b10467484d2e5e
jPales1 <joaopedropales@gmail.com>
2024-11-06 23:33:40 -0300

    atualizar serviço de lista de compras para usar uma URL base e adicionar métodos para gerenciar usuários

--- src/app/shopping-list.service.ts
+++ src/app/shopping-list.service.ts
@@ -7,19 +7,31 @@ import { Item } from './item.model';
   providedIn: 'root'
 })
 export class ShoppingListService {
-  private apiUrl = 'http://localhost:3000/shopping-list';
+  private apiUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) {}
 
   getItems(): Observable<Item[]> {
-    return this.http.get<Item[]>(this.apiUrl);
+    return this.http.get<Item[]>(`${this.apiUrl}/shopping-list`);
   }
 
   addItem(item: Item): Observable<Item> {
-    return this.http.post<Item>(this.apiUrl, item);
+    return this.http.post<Item>(`${this.apiUrl}/shopping-list`, item);
   }
 
   removeItem(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/shopping-list/${id}`);
+  }
+
+  addUser(user: { id: string, name: string, email: string }): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/users`, user);
+  }
+
+  getUser(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/users/${id}`);
+  }
+
+  updateItem(item: Item): Observable<Item> {
+    return this.http.put<Item>(`${this.apiUrl}/shopping-list/${item.id}`, item);
   }
 }
